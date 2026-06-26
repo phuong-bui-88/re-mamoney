e249af9b7b86cea1896cbb1120865033f70acfa8
Test User <test@example.com>
2026-06-26 10:43:32 +0700

    remove build

--- android/app/src/main/kotlin/com/example/mamoney/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mamoney
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/services/logging_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:logging/logging.dart';
-
-void setupLogging() {
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((record) {
-    // ignore: avoid_print
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-}
